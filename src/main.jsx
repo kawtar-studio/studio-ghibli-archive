aae9bd04cdb4ce4c0d632730158bf9782cd0b270
Kawtar <barakat.kawtarr@gmail.com>
2026-07-24 19:50:01 +0100

    Create scalable project architecture

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./styles/reset.css";
+import "./styles/variables.css";
+import "./styles/typography.css";
+import "./styles/layout.css";
+import "./styles/components.css";
+import "./styles/animations.css";
+import "./styles/global.css";
+import "./styles/responsive.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
