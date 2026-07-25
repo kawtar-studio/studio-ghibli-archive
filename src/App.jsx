8ce3ca323bf3fb44ab550f0fe7fbc72fcc4d1440
Kawtar <barakat.kawtarr@gmail.com>
2026-07-25 01:15:43 +0100

    Simplify project structure and remove conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home/Home";
 
+
 export default function App() {
+
     return (
+
         <MainLayout>
+
             <Home />
+
         </MainLayout>
+
     );
+
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import "./styles/reset.css";
-import "./styles/variables.css";
-import "./styles/typography.css";
-import "./styles/layout.css";
-import "./styles/components.css";
-import "./styles/animations.css";
-import "./styles/responsive.css";
 import "./styles/global.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+ReactDOM.createRoot(
+    document.getElementById("root")
+).render(
+
     <React.StrictMode>
         <App />
     </React.StrictMode>
+
 );
